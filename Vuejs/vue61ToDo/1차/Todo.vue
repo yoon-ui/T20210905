63771fd72d0e9310bfa780b949e95298e2f490a4
yoon ah young <21dkdud21@gmail.com>
2021-09-05 12:15:00 +0900

    시작

---
+++ Vuejs/vue61ToDo/1차/Todo.vue
@@ -0,0 +1,33 @@
+<style scoped>
+body {
+    text-align: center;
+    background-color: #f6f6f8;
+}
+input {
+    border-style: groove;
+    width: 200px;
+}
+button {
+    border-style: groove;
+}
+.shadow {
+    box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
+</style>
+
+<template>
+    <div id="app">
+        <!-- TodoHeader -->
+        <TodoHeader></TodoHeader>
+
+        <!-- TodoInput -->
+        <TodoInput></TodoInput>
+
+        <!-- TodoList -->
+        <TodoList></TodoList>
+
+        <!-- TodoFooter -->
+        <TodoFooter></TodoFooter>
+    </div>
+</template>
+
